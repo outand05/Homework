d4f6947215f941b365e44fd02d7ed30ff11033b5
outand05 <115588949+outand05@users.noreply.github.com>
2022-10-18 01:48:13 -0300

    prueba de funciones

--- and1.js
+++ and1.js
@@ -1,1 +1,19 @@
+function presentacion (nombre, edad) {
+  		console.log("hola, mi nombre es " + nombre);
+  		console.log("y tengo " + edad);
+  	}
 
+
+function PuedeManejar (Edad);
+{
+
+	if (edad > 18);{
+		console.log	("Esta acta para manejar");
+	} else { if (edad < 18);{
+		console.log("No esta acta para manejar");
+		
+	}
+	
+
+
+	
